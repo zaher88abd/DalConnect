c8fee44780a8fd61d41a9eddf43ab618c44d5d13
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-02-10 14:35:16 -0400

    Building list
    Create the layout and the activity for it.

---
+++ app/src/main/java/ca/connect/dal/dalconnect/ListViewActivity.java
@@ -0,0 +1,12 @@
+package ca.connect.dal.dalconnect;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zaher on 2018-02-10.
+ * this activity for buildings list and direct to google map application.
+ */
+
+public class ListViewActivity extends AppCompatActivity {
+
+}
