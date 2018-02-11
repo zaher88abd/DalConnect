2ccfedd5b51569019378846a55b4ddbb313263a5
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-02-11 11:34:39 -0400

    Fix the LocationListener

--- app/src/main/java/ca/connect/dal/dalconnect/ListViewActivity.java
+++ app/src/main/java/ca/connect/dal/dalconnect/ListViewActivity.java
@@ -27,7 +27,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
  * this activity for buildings list and direct to google map application.
  */
 
-public class ListViewActivity extends AppCompatActivity {
+public class ListViewActivity extends AppCompatActivity implements LocationListener {
     private ListView lvBuildings;
     ArrayAdapter<String> buildingAdapter;
     String[] buildings;
@@ -38,36 +38,13 @@ public class ListViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.buildings_list);
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                userLocation = location;
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-            }
-        };
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}, 101);
-            return;
-        } else {
-            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
-        }
+        } else
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
 
         lvBuildings = findViewById(R.id.lvBuildings);
         buildingAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
@@ -101,6 +78,35 @@ public class ListViewActivity extends AppCompatActivity {
                 startActivity(Intent.createChooser(intent, "Select an application"));
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}, 101);
+        } else
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        userLocation = location;
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
 
