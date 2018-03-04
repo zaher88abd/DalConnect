b332d6aca282137084c47298c8e52ce3f60a2bec
Henry Ugokwe <Henry Ugokwe>
2018-03-04 11:34:53 -0600

    Added two classes, MassageAdapter and MessageData Class. Also the MainActivitiy.java and activity_main.xml was renamed to ChatActivity and activity_chat.xml respectively

---
+++ app/src/main/java/com/project/dalconnect/chatbot/MessageData.java
@@ -0,0 +1,16 @@
+package com.project.dalconnect.chatbot;
+
+/**
+ * Created by hugokwe on 3/1/2018.
+ * This data object class hold the chat interactions between google and user from the listView
+ *
+ */
+public class MessageData {
+    public boolean isUser;
+    String messageBody;
+    //Constructor
+    public MessageData (boolean isUSER, String chatMessage, String Sender, String Receiver ) {
+        messageBody = chatMessage;
+        isUser = isUSER;
+    }
+}
