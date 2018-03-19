17df73cb3344f8e7f27ffa45201f9a4942a221ef
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-03-19 14:14:15 -0300

    Read the context

--- app/src/main/java/ca/connect/dal/dalconnect/ChatActivity.java
+++ app/src/main/java/ca/connect/dal/dalconnect/ChatActivity.java
@@ -1,30 +1,40 @@
 package ca.connect.dal.dalconnect;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.os.AsyncTask;
-        import android.text.TextUtils;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.EditText;
-        import android.widget.ImageButton;
-        import android.widget.ListView;
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
-        import java.util.ArrayList;
+import com.google.gson.JsonElement;
 
-        import ai.api.AIConfiguration;
-        import ai.api.AIDataService;
-        import ai.api.AIServiceException;
-        import ai.api.model.AIError;
-        import ai.api.model.AIRequest;
-        import ai.api.model.AIResponse;
-        import ai.api.model.Metadata;
-        import ai.api.model.Result;
-        import ai.api.model.Status;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ai.api.AIConfiguration;
+import ai.api.AIDataService;
+import ai.api.AIServiceException;
+import ai.api.RequestExtras;
+import ai.api.model.AIContext;
+import ai.api.model.AIError;
+import ai.api.model.AIOutputContext;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+import ai.api.model.Metadata;
+import ai.api.model.Result;
+import ai.api.model.Status;
 
 
 public class ChatActivity extends AppCompatActivity {
-    
+
     public static final String LOG_TAG = ChatActivity.class.getName();
 
     ListView conversationList;
@@ -44,19 +54,19 @@ public class ChatActivity extends AppCompatActivity {
         final AIConfiguration.SupportedLanguages lang =
                 AIConfiguration.SupportedLanguages.fromLanguageTag("en");
 
-        aiConfiguration = new AIConfiguration("0436e0e5611a4ddab3baee87314b2444",lang);
+        aiConfiguration = new AIConfiguration("0436e0e5611a4ddab3baee87314b2444", lang);
         aiDataService = new AIDataService(aiConfiguration);
 
-        sendKey = (ImageButton)findViewById(R.id.imageButton);
-        inputText = (EditText)findViewById(R.id.textMessage);
+        sendKey = findViewById(R.id.imageButton);
+        inputText = findViewById(R.id.textMessage);
 
         sendKey.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String text = inputText.getText().toString();
-                if(!text.trim().equals("")) {
-                    addMessage(text,true);
+                if (!text.trim().equals("")) {
+                    addMessage(text, true);
                     inputText.setText("");
                     sendRequest(text);
                 }
@@ -64,11 +74,11 @@ public class ChatActivity extends AppCompatActivity {
         });
 
 
-        conversationList = (ListView) findViewById(R.id.ConversationList);
+        conversationList = findViewById(R.id.ConversationList);
         conversationList.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
         conversationList.setStackFromBottom(true);
 
-        messageList = new ArrayList<MessageData>();
+        messageList = new ArrayList<>();
         messageAdapter = new MessageAdapter(this, messageList);
         conversationList.setAdapter(messageAdapter);
 
@@ -77,7 +87,7 @@ public class ChatActivity extends AppCompatActivity {
 
     private void addMessage(String message, boolean isUser) {
 
-        final MessageData chatMessage = new MessageData(isUser, "", "","");
+        final MessageData chatMessage = new MessageData(isUser, "", "", "");
         chatMessage.messageBody = message;
         messageAdapter.add(chatMessage);
         messageAdapter.notifyDataSetChanged();
@@ -87,7 +97,7 @@ public class ChatActivity extends AppCompatActivity {
 
         final String contextString = String.valueOf(userText);
 
-        final AsyncTask<String, Void, AIResponse> task = new AsyncTask<String, Void, AIResponse>() {
+        @SuppressLint("StaticFieldLeak") final AsyncTask<String, Void, AIResponse> task = new AsyncTask<String, Void, AIResponse>() {
 
             private AIError aiError;
 
@@ -124,6 +134,8 @@ public class ChatActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
+
+
                 Log.d(LOG_TAG, "onResult");
 
                 Log.i(LOG_TAG, "Received success response");
@@ -134,6 +146,17 @@ public class ChatActivity extends AppCompatActivity {
                 Log.i(LOG_TAG, "Status type: " + status.getErrorType());
 
                 final Result result = response.getResult();
+                List<AIOutputContext> contextList = result.getContexts();
+                ArrayList<String> contextNames = new ArrayList<>();
+                for (AIOutputContext context : contextList) {
+                    Log.i(LOG_TAG, "context name: " + context.getName());
+                    contextNames.add(context.getName());
+                }
+                if(contextNames.contains("going")&&contextNames.contains("car")){
+                    findFastRout();
+                }
+
+
                 Log.i(LOG_TAG, "Resolved query: " + result.getResolvedQuery());
 
                 Log.i(LOG_TAG, "Action: " + result.getAction());
@@ -142,17 +165,18 @@ public class ChatActivity extends AppCompatActivity {
                 Log.i(LOG_TAG, "Speech: " + speech);
 
                 if (!TextUtils.isEmpty(speech))
-                    addMessage(speech,false);
+                    addMessage(speech, false);
 
                 final Metadata metadata = result.getMetadata();
-                System.out.println(metadata+"metadata Found+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                System.out.println(metadata + "metadata Found+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
                 if (metadata != null) {
                     Log.i(LOG_TAG, "Intent id: " + metadata.getIntentId());
                     Log.i(LOG_TAG, "Intent name: " + metadata.getIntentName());
                     System.out.println("Intent id: " + metadata.getIntentId());
                     System.out.println("Intent name: " + metadata.getIntentName());
-                }else{
+//                    Log.d(LOG_TAG,response.)
+                } else {
                     System.out.println("metadata not Found+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
                 }
@@ -160,11 +184,16 @@ public class ChatActivity extends AppCompatActivity {
 
         });
     }
+
+    private void findFastRout() {
+        Toast.makeText(this, "Find Fast Rout", Toast.LENGTH_SHORT).show();
+    }
+
     private void onError(final AIError error) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                addMessage(error.toString(),false);
+                addMessage(error.toString(), false);
             }
         });
     }
