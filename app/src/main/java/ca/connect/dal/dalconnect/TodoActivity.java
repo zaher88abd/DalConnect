73fdb5e1321cab2545eabfea645e1c2887e3b859
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-03-30 19:22:20 -0300

    Change the activity_main to activity_todo

--- app/src/main/java/ca/connect/dal/dalconnect/TodoActivity.java
+++ app/src/main/java/ca/connect/dal/dalconnect/TodoActivity.java
@@ -54,7 +54,7 @@ public class TodoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_todo);
 
         db = FirebaseFirestore.getInstance();
         dialog = new SpotsDialog(this);
