64b495caeab6cecf85fe78dc31db99537680a9e8
gaoyounan <gaoyounan@foxmail.com>
2018-04-01 11:08:32 -0300

    Polish the look of Chat Part

--- app/src/main/java/ca/connect/dal/dalconnect/chat/ChatListAdapter.java
+++ app/src/main/java/ca/connect/dal/dalconnect/chat/ChatListAdapter.java
@@ -8,8 +8,12 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import ca.connect.dal.dalconnect.R;
 import ca.connect.dal.dalconnect.chat.model.Message;
@@ -57,6 +61,11 @@ public class ChatListAdapter extends BaseAdapter
         Message message = (Message) getItem(i);
         String idSender = message.getIdSender();
 
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT-3"));
+        java.util.Date date = new Date(message.getTimestamp());
+        String str = sdf.format(date);
+
         if(idSender.equals(AuthUtils.getInstance().getCurrentUId()))
         {
             //myself
@@ -65,6 +74,8 @@ public class ChatListAdapter extends BaseAdapter
 
             ImageView my_portrait = (ImageView) view.findViewById(R.id.id_image_myself);
             TextView chat_text = (TextView) view.findViewById(R.id.id_chat_myself);
+            TextView time_text_my = (TextView) view.findViewById(R.id.id_chat_time);
+            time_text_my.setText(str);
 
             BitmapDrawable bitmapDrawable = PortraitUtils.getInstance().getPortraitByName(message.getSenderPortrait());
 
@@ -87,6 +98,9 @@ public class ChatListAdapter extends BaseAdapter
                     R.layout.chat_list_view_items_left, null);
             ImageView other_portrait = (ImageView) view.findViewById(R.id.id_image_other);
             TextView chat_text = (TextView) view.findViewById(R.id.id_chat_text_other);
+
+            TextView time_text_other = (TextView) view.findViewById(R.id.id_chat_time_other);
+            time_text_other.setText(str);
 
             BitmapDrawable bitmapDrawable = PortraitUtils.getInstance().getPortraitByName(message.getSenderPortrait());
 
